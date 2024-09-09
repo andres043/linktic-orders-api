7e79e0c176ec8358b102daf6289593cbd5c4d9d7
Andres Leal <andres.leal@MacBook-Pro-de-Andres.local>
2024-09-09 09:10:46 -0500

    Adds CrossOrigin and license

--- src/main/java/co/com/linktic/linkticordersapi/adapters/input/web/ListOrdersController.java
+++ src/main/java/co/com/linktic/linkticordersapi/adapters/input/web/ListOrdersController.java
@@ -2,8 +2,11 @@ package co.com.linktic.linkticordersapi.adapters.input.web;
 
 import co.com.linktic.linkticordersapi.adapters.input.web.dto.Order;
 import co.com.linktic.linkticordersapi.domain.usecases.ListOrdersUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +16,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@Tag(name = "Order", description = "API for managing orders")
 public class ListOrdersController {
 
     private final ListOrdersUseCase listOrdersUseCase;
 
     @GetMapping("/orders")
+    @Operation(summary = "List orders", description = "Returns a list of orders.")
     public ResponseEntity<List<Order>> listOrders() {
         List<Order> orders = listOrdersUseCase.listOrders()
                 .stream().map(order -> new Order(order.orderId(), order.product(), order.quantity(), order.price()))
